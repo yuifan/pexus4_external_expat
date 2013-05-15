e78e27beaffcabdebef49f281736836dc6dcefad
U-yuifan-PC\yuifan <yuifan@yuifan-PC.(none)>
2013-05-14 22:46:15 -0700

    update 4.2

--- Android.mk
+++ Android.mk
@@ -8,7 +8,12 @@ common_SRC_FILES := \
 	lib/xmlrole.c \
 	lib/xmltok.c
 
-common_CFLAGS := -Wall -Wmissing-prototypes -Wstrict-prototypes -fexceptions -DHAVE_EXPAT_CONFIG_H
+common_CFLAGS := \
+    -Wall \
+    -Wmissing-prototypes -Wstrict-prototypes \
+    -Wno-unused-parameter -Wno-missing-field-initializers \
+    -fexceptions \
+    -DHAVE_EXPAT_CONFIG_H
 
 common_C_INCLUDES += \
 	$(LOCAL_PATH)/lib
@@ -32,15 +37,11 @@ ifeq ($(HOST_OS),darwin)
 endif
 
 LOCAL_MODULE:= libexpat
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_MODULE_TAGS := optional
-LOCAL_COPY_HEADERS_TO := $(common_COPY_HEADERS_TO)
-LOCAL_COPY_HEADERS := $(common_COPY_HEADERS)
 
 include $(BUILD_HOST_STATIC_LIBRARY)
 
-
-# For the device
-# =====================================================
 
 include $(CLEAR_VARS)
 
@@ -48,10 +49,54 @@ LOCAL_SRC_FILES := $(common_SRC_FILES)
 LOCAL_CFLAGS += $(common_CFLAGS)
 LOCAL_C_INCLUDES += $(common_C_INCLUDES)
 
+ifeq ($(HOST_OS),darwin)
+	LOCAL_CFLAGS += -fno-common
+endif
+
+LOCAL_MODULE:= libexpat-host
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
+LOCAL_MODULE_TAGS := optional
+LOCAL_COPY_HEADERS_TO := $(common_COPY_HEADERS_TO)
+LOCAL_COPY_HEADERS := $(common_COPY_HEADERS)
+
+include $(BUILD_HOST_SHARED_LIBRARY)
+
+
+# For the device
+# =====================================================
+
+# Device static library
+include $(CLEAR_VARS)
+
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_SDK_VERSION := 8
+endif
+
+LOCAL_SRC_FILES := $(common_SRC_FILES)
+LOCAL_CFLAGS += $(common_CFLAGS)
+LOCAL_C_INCLUDES += $(common_C_INCLUDES)
+
+LOCAL_MODULE:= libexpat_static
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_STATIC_LIBRARY)
+
+# Device shared library
+include $(CLEAR_VARS)
+
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_SDK_VERSION := 8
+endif
+
+LOCAL_SRC_FILES := $(common_SRC_FILES)
+LOCAL_CFLAGS += $(common_CFLAGS)
+LOCAL_C_INCLUDES += $(common_C_INCLUDES)
+
 LOCAL_MODULE:= libexpat
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_MODULE_TAGS := optional
 LOCAL_COPY_HEADERS_TO := $(common_COPY_HEADERS_TO)
 LOCAL_COPY_HEADERS := $(common_COPY_HEADERS)
 
 include $(BUILD_SHARED_LIBRARY)
-
